46e98f0d40ad406bfbda3ed88d1081de39cd3106
quentin <clavier.quentin@gmail.com>
2022-10-02 18:41:38 +0200

    commit before checkout

---
+++ app/Models/Categorie.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categorie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nom_categorie',
+        'url_image_categorie'
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
