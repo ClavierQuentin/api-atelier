c76cfcf32e3f17cf713642de130e86af8632067e
quentin <clavier.quentin@gmail.com>
2022-10-21 18:16:28 +0200

    work on blade template(

--- resources/views/newsletter/edit_email.blade.php
+++ resources/views/newsletter/edit_email.blade.php
@@ -1,3 +1,25 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+
+    {{-- Composants pour configurer l'éditeur de texte --}}
+    @include('components.head.tinymce-config')
+
+</head>
+
+
 <form method="POST" class="custom-form" action="{{ route('email.delete') }}">
     @csrf
     <div class="form-group">
@@ -6,3 +28,5 @@
     </div>
     <button>Se désinscrire</button>
 </form>
+
+</html>
